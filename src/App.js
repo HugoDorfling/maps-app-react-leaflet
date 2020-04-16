28dffd8cdc54c48975e2a95370b62693f42ff13e
HugoDorfling <hugodorfling@live.co.za>
2020-04-16 05:29:33 +0200

    show map & custom icon leaflet

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// @flow
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import { Map, TileLayer, Marker, Popup } from "react-leaflet";
+import L from "leaflet";
+import "./App.css";
+
+var needIcon = L.icon({
+  iconUrl:
+    "https://drive.google.com/uc?export=view&id=11aK61HHCL1TrdFfgt0Y-2hcYc6kiRVnw",
+  iconSize: [40, 47],
+  iconAnchor: [22, 94],
+  popupAnchor: [-3, -76],
+});
+
+export default class App extends Component {
+  state = {
+    lat: 51.505,
+    lng: -0.09,
+    zoom: 13,
+  };
+
+  render() {
+    const position = [this.state.lat, this.state.lng];
+    return (
+      <Map className="map" center={position} zoom={this.state.zoom}>
+        <TileLayer
+          attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker icon={needIcon} position={position}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </Map>
+    );
+  }
 }
-
-export default App;
